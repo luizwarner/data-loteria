09fffeb6b5ac5c67257d5d07b17effdc0d2226c9
luizwarner <luizwarner@gmail.com>
2020-04-13 18:37:30 -0300

    primeiro commit

---
+++ src/main/java/br/com/loteria/DataLoteriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.loteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLoteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLoteriaApplication.class, args);
+	}
+
+}
